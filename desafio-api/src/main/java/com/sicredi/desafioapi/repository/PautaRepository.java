30de3806e574e26d823cf818df25f007525d4999
cmeira <cmeiraa@gmail.com>
2024-11-27 11:59:09 -0300

    Delete desafio-api directory

--- desafio-api/src/main/java/com/sicredi/desafioapi/repository/PautaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sicredi.desafioapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sicredi.desafioapi.domain.Pauta;
-
-public interface PautaRepository extends JpaRepository<Pauta, Long> {
-}
